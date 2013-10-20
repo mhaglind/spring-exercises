d8c4a1f53e54a71888715a18f818ee9c7dfae584
Mårten Haglind <marten@haglind.com>
2013-10-20 23:22:10 +0200

    Formatting

--- labs/xml-config/start/src/test/java/no/arktekk/training/spring/DependencyInjectionTest.java
+++ labs/xml-config/start/src/test/java/no/arktekk/training/spring/DependencyInjectionTest.java
@@ -1,85 +1,83 @@
 package no.arktekk.training.spring;
 
-import no.arktekk.training.spring.domain.Auction;
-import no.arktekk.training.spring.repository.AuctionRepository;
-import no.arktekk.training.spring.service.AuctionService;
-import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
+import no.arktekk.training.spring.repository.AuctionRepository;
+import no.arktekk.training.spring.service.AuctionService;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
 
 /**
  * @author <a href="mailto:kaare.nilsen@arktekk.no">Kaare Nilsen</a>
+ * @author <a href="mailto:marten@haglind.com">Marten Haglind</a>
  */
 public class DependencyInjectionTest {
 
+	/**
+	 * Configure necessary bean in applicationContext.xml and complete the
+	 * helper methods in order to get this test to run.
+	 * <p/>
+	 * TIP: The repository implementation is:
+	 * no.arktekk.training.spring.repository.impl.StubAuctionRepository
+	 */
+	@Test
+	public void step_1() {
+		ApplicationContext ctx = createSpringContainer();
+		assertNotNull(ctx);
+		AuctionRepository auctionRepository = lookupAuctionRepository(ctx);
+		assertNotNull(auctionRepository);
+	}
 
-    /**
-     * Configure neccesary bean in applicationContext.xml and complete the
-     * helper methods in order to get this test to run.
-     * <p/>
-     * TIP: The repository implementation is:
-     * no.arktekk.training.spring.repository.impl.StubAuctionRepository
-     */
-    @Test public void step_1() {
-        ApplicationContext ctx = createSpringContainer();
-        assertNotNull(ctx);
-        AuctionRepository auctionRepository = lookupAuctionRepository(ctx);
-        assertNotNull(auctionRepository);
-    }
+	/**
+	 * Configure necessary beans in applicationContext.xml and complete the
+	 * helper methods in order to get this test to run.
+	 * <p/>
+	 * TIP: The repository implementation is:
+	 * no.arktekk.training.spring.service.impl.DefaultAuctionService TIP: The
+	 * service needs the repository in order to work.
+	 */
+	@Test
+	public void step_2() {
+		ApplicationContext ctx = createSpringContainer();
+		AuctionService auctionService = lookupAuctionService(ctx);
+		assertNotNull(auctionService);
+	}
 
+	/**
+	 * Complete TODO's Replace all calls to fail() with proper test code
+	 */
+	@Test
+	public void step_3() {
 
-    /**
-     * Configure neccesary beans in applicationContext.xml and complete the
-     * helper methods in order to get this test to run.
-     * <p/>
-     * TIP: The repository implementation is:
-     * no.arktekk.training.spring.service.impl.DefaultAuctionService
-     * TIP: The service needs the repository in order to work.
-     */
-    @Test public void step_2() {
-        ApplicationContext ctx = createSpringContainer();
-        AuctionService auctionService = lookupAuctionService(ctx);
-        assertNotNull(auctionService);
-    }
+		ApplicationContext ctx = createSpringContainer();
+		AuctionRepository auctionRepository = lookupAuctionRepository(ctx);
+		AuctionService auctionService = lookupAuctionService(ctx);
 
-    /**
-     * Complete TODO's
-     * Replace all calls to fail() with proper test code
-     */
-    @Test public void step_3() {
+		// TODO: Check that there are no running auctions present using the
+		// allRunningAuctions() in AuctionService
+		fail();
 
-        ApplicationContext ctx = createSpringContainer();
-        AuctionRepository auctionRepository = lookupAuctionRepository(ctx);
-        AuctionService auctionService = lookupAuctionService(ctx);
+		// TODO: Add 2 new Auction objects, using the auctionRepository
+		fail();
 
-        //TODO: Check that there are no running auctions present using the allRunningAuctions() in AuctionService
-        fail();
+		// TODO: Check that there are now 2 running auctions
+		fail();
 
-        //TODO: Add 2 new Auction objects, using the auctionRepository
-        fail();
+		// TODO: Use the auction service to query by id, and verify that the
+		// result is as expected
+		fail();
+	}
 
-        //TODO: Check that there are now 2 running auctions
-        fail();
+	private ApplicationContext createSpringContainer() {
+		return null;
+	}
 
-        //TODO: Use the auction service to query by id, and verify that the result is as expected
-        fail();
-    }
+	private AuctionRepository lookupAuctionRepository(ApplicationContext ctx) {
+		return null;
+	}
 
-
-    private ApplicationContext createSpringContainer() {
-        return null;
-    }
-
-
-    private AuctionRepository lookupAuctionRepository(ApplicationContext ctx) {
-        return null;
-    }
-
-    private AuctionService lookupAuctionService(ApplicationContext ctx) {
-        return null;
-    }
+	private AuctionService lookupAuctionService(ApplicationContext ctx) {
+		return null;
+	}
 }
