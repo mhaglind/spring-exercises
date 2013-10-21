4ab34dba9ffc5838e590f71f437e7358796c026b
Mårten Haglind <marten@haglind.com>
2013-10-21 15:56:54 +0200

    Added empty project. For daily reviews and improv demos.

---
+++ demos/review/src/main/java/com/haglind/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.haglind.demo;
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// ApplicationContext context
+		// = new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+
+}
