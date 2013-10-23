7aef133d07ac2d02b449fef85e6ba0cdd0d2a434
Mårten Haglind <marten@haglind.com>
2013-10-23 14:35:54 +0200

    Replaced deprecations. SimpleJdbcTemplate and queryForInt.

--- labs/transaction-management/start/src/main/java/no/arktekk/training/spring/repository/impl/JdbcAuctionRepository.java
+++ labs/transaction-management/start/src/main/java/no/arktekk/training/spring/repository/impl/JdbcAuctionRepository.java
@@ -1,51 +1,53 @@
 package no.arktekk.training.spring.repository.impl;
 
-import no.arktekk.training.spring.domain.Auction;
-import no.arktekk.training.spring.mapper.AuctionMapper;
-import no.arktekk.training.spring.repository.AuctionRepository;
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
-import org.springframework.stereotype.Repository;
-
-import javax.sql.DataSource;
-import java.util.List;
-
 import static no.arktekk.training.spring.util.DatabaseUtils.no_NO;
 import static no.arktekk.training.spring.util.DatabaseUtils.timeStampFormatter;
 
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import no.arktekk.training.spring.domain.Auction;
+import no.arktekk.training.spring.mapper.AuctionMapper;
+import no.arktekk.training.spring.repository.AuctionRepository;
+
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author <a href="mailto:kaare.nilsen@arktekk.no">Kaare Nilsen</a>
+ * @author <a href="mailto:marten@haglind.com">Marten Haglind</a>
  */
 @Repository
 public class JdbcAuctionRepository implements AuctionRepository {
-    private final SimpleJdbcTemplate template;
+	private final JdbcTemplate template;
 
-    @Autowired
-    public JdbcAuctionRepository(DataSource dataSource) {
-        this.template = new SimpleJdbcTemplate(dataSource);
-    }
+	@Autowired
+	public JdbcAuctionRepository(DataSource dataSource) {
+		this.template = new JdbcTemplate(dataSource);
+	}
 
-    public List<Auction> listAllRunningAuctions() {
-        DateTime now = new DateTime();
-        return template.query(
-                "select * from Auctions where ? between starts and expires",
-                new AuctionMapper(),
-                timeStampFormatter.print(now.toDate(), no_NO));
-    }
+	public List<Auction> listAllRunningAuctions() {
+		DateTime now = new DateTime();
+		return template.query(
+				"select * from Auctions where ? between starts and expires",
+				new AuctionMapper(),
+				timeStampFormatter.print(now.toDate(), no_NO));
+	}
 
-    public Auction findById(Double auctionId) {
-        return template.queryForObject("select * from Auctions where id = ?", new AuctionMapper(), auctionId);
-    }
+	public Auction findById(Double auctionId) {
+		return template.queryForObject("select * from Auctions where id = ?",
+				new AuctionMapper(), auctionId);
+	}
 
-    @Override
-    public void store(Auction auction) {
-        template.update("insert into Auctions values(?,?,?,?,?)",
-                auction.id(),
-                auction.minimumPrice(),
-                timeStampFormatter.print(auction.starts().toDate(), no_NO),
-                timeStampFormatter.print(auction.expires().toDate(), no_NO),
-                auction.description());
-    }
+	@Override
+	public void store(Auction auction) {
+		template.update("insert into Auctions values(?,?,?,?,?)", auction.id(),
+				auction.minimumPrice(),
+				timeStampFormatter.print(auction.starts().toDate(), no_NO),
+				timeStampFormatter.print(auction.expires().toDate(), no_NO),
+				auction.description());
+	}
 }
--- labs/transaction-management/start/src/test/java/no/arktekk/training/spring/repository/TransactionManagementTest.java
+++ labs/transaction-management/start/src/test/java/no/arktekk/training/spring/repository/TransactionManagementTest.java
@@ -1,7 +1,12 @@
 package no.arktekk.training.spring.repository;
 
+import static org.junit.Assert.assertEquals;
+
+import javax.sql.DataSource;
+
 import no.arktekk.training.spring.domain.Auction;
 import no.arktekk.training.spring.service.AuctionService;
+
 import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,46 +17,44 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.AnnotationTransactionAttributeSource;
 
-import javax.sql.DataSource;
-
-import static org.junit.Assert.assertEquals;
-
 /**
  * @author <a href="mailto:kaare.nilsen@arktekk.no">Kaare Nilsen</a>
+ * @author <a href="mailto:marten@haglind.com">MŒrten Haglind</a>
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
 public class TransactionManagementTest {
 
-    @Autowired
-    private PlatformTransactionManager transactionManager;
+	@Autowired
+	private PlatformTransactionManager transactionManager;
 
-    @Autowired
-    private AnnotationTransactionAttributeSource annotationTransactionAttributeSource;
+	@Autowired
+	private AnnotationTransactionAttributeSource annotationTransactionAttributeSource;
 
-    @Autowired
-    private AuctionService auctionService;
+	@Autowired
+	private AuctionService auctionService;
 
-    @Autowired
-    private DataSource dataSource;
+	@Autowired
+	private DataSource dataSource;
 
+	/**
+	 * Register transaction manager, and add neccecary transaction attributes in
+	 * order to get this test to run
+	 */
+	@Test
+	public void step_1() throws Exception {
 
-    /**
-     * Register transaction manager, and add neccecary
-     * transaction attributes in order to get this test to run
-     */
-    @Test public void step_1() throws Exception {
+		int auctionsBeforeTest = auctionCount();
 
-        int auctionsBeforeTest = auctionCount();
+		auctionService.newAuction(new Auction(1D, 100D, "Mint Progrog Records",
+				new DateTime(), new DateTime().plusDays(10)));
 
-        auctionService.newAuction(new Auction(1D, 100D, "Mint Progrog Records", new DateTime(), new DateTime().plusDays(10)));
+		assertEquals(auctionsBeforeTest + 1, auctionCount());
+	}
 
-        assertEquals(auctionsBeforeTest + 1, auctionCount());
-    }
-
-
-    private int auctionCount() {
-        return new JdbcTemplate(dataSource).queryForInt("select count(*) from Auctions");
-    }
+	private int auctionCount() {
+		return new JdbcTemplate(dataSource).queryForObject(
+				"select count(*) from Auctions", Integer.class);
+	}
 
 }
