ad87e62984e2801966c6c9b958ded79ef0fd53bd
mhaglind <marten@haglind.com>
2016-01-21 13:35:48 +0100

    Spring Amqp demo, initial commit.

---
+++ demos/amqp-demo/src/main/java/com/haglind/AmqpDemoApplication.java
@@ -0,0 +1,12 @@
+package com.haglind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpDemoApplication.class, args);
+	}
+}
---
+++ demos/amqp-demo/src/test/java/com/haglind/AmqpDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haglind;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AmqpDemoApplication.class)
+public class AmqpDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
