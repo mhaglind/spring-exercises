0a64b68376bd26c0e43e0a486747ae03355a7a00
Mårten Haglind <marten@haglind.com>
2016-02-05 13:51:38 +0100

    Added json support to rest-solution

--- labs/rest/solution/src/main/java/no/arktekk/training/spring/controller/RestAuctionController.java
+++ labs/rest/solution/src/main/java/no/arktekk/training/spring/controller/RestAuctionController.java
@@ -11,12 +11,15 @@ import no.arktekk.training.spring.domain.AuctionList;
 import no.arktekk.training.spring.service.AuctionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -48,7 +51,17 @@ public class RestAuctionController {
 		return mav;
 	}
 
+	@RequestMapping(method = RequestMethod.GET, 
+			value = "/auctions/{auctionId}.json",
+			produces = "application/json")
+	@ResponseBody
+	public Auction getAuctionJson(@PathVariable String auctionId) {
+		Auction auction = auctionService.findById(auctionId);
+		return auction;
+	}
+	
 	@RequestMapping(method = RequestMethod.POST, value = "/auctions")
+	@ResponseStatus(HttpStatus.CREATED)
 	public ModelAndView createAuction(@RequestBody String auctionXml) {
 		
 		Source source = new StreamSource(new StringReader(auctionXml));
