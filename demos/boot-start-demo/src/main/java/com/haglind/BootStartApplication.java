dfd5da4b4fdb71d7efe69ab230755da43ff02829
Mårten Haglind <marten@haglind.com>
2016-09-15 09:14:34 +0200

    Controller instead of RestController

--- demos/boot-start-demo/src/main/java/com/haglind/BootStartApplication.java
+++ demos/boot-start-demo/src/main/java/com/haglind/BootStartApplication.java
@@ -2,15 +2,16 @@ package com.haglind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableAutoConfiguration
-@RestController
+@Controller
 public class BootStartApplication {
 
 	@RequestMapping(value = "/")
-	public String helloWorld() {
+	public @ResponseBody String helloWorld() {
 		return "Hello world of Spring Boot!";
 	}
 
